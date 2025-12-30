9e357b958b21e518d943e7fee20d01b2a49e645d
iam-abhinavnarayan <abhinavnarayanwork@gmail.com>
2025-12-30 18:45:56 +0530

    Arrays : Rotate Array

---
+++ Arrays/rotate_array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int rotateArr(vector<int> &arr, int d)
+{
+
+    int n = arr.size();
+
+    d = d % n;
+
+    reverse(arr.begin(), arr.begin()+d);
+    reverse(arr.begin()+d, arr.end());
+    reverse(arr.begin(), arr.end());
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int d = 2;
+
+    rotateArr(arr, d);
+
+    for (int num : arr)
+    {
+        cout << num << " ";
+    }
+    return 0;
+}
