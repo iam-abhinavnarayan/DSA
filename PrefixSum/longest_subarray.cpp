f450d3a3865663d11db7c344cdae2077809ea25c
iam-abhinavnarayan <abhinavnarayanwork@gmail.com>
2026-01-08 10:15:22 +0530

    PrefixSum : Longest Subarray With Sum K

--- PrefixSum/longest_subarray.cpp
+++ PrefixSum/longest_subarray.cpp
@@ -2,30 +2,40 @@
 // subarray having sum k using nested loop
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+
 using namespace std;
 
 // Function to find longest sub-array having sum k
 int longestSubarray(vector<int>& arr, int k) {
-    int res = 0;
-
-    for (int i = 0; i < arr.size(); i++) {
-        
-        // Sum of subarray from i to j
-        int sum = 0;
-        for (int j = i; j < arr.size(); j++) {
-            sum += arr[j];
-          
-          	// If subarray sum is equal to k
-            if (sum == k) {
-              
-              	// find subarray length and update result
-              	int subLen = j - i + 1;
-                res = max(res, subLen);
-            }
-        }
-    }
-
-    return res;
+    int len=0;
+       int n=arr.size();
+       
+       vector<int> prefixSum(n,0);
+       prefixSum[0]=arr[0];
+       for(int i=1; i<n; i++){
+          prefixSum[i]=prefixSum[i-1]+arr[i];
+       }
+       
+       unordered_map<int,int> m;//PrefixSum = first  Index
+       
+       for(int j=0; j<n; j++){
+           
+           //case1: subarray from 0 to j.
+           if(prefixSum[j]==k){
+               len=max(len,j+1);
+           }
+           //case2: subarray ending at j.
+           int val=prefixSum[j]-k;
+           if(m.find(val)!=m.end()){
+               len=max(len,j-m[val]);
+           }
+           //store first occurrence only
+           if(m.find(prefixSum[j])==m.end()){
+               m[prefixSum[j]]=j;
+           }
+       }
+       return len;
 }
 
 int main() {
