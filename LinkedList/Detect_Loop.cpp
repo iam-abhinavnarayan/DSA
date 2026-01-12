ad033021697a5f1b15f13d8fd58e23f65eda5602
iam-abhinavnarayan <abhinavnarayanwork@gmail.com>
2026-01-12 21:18:01 +0530

    Linked List : Detect Loop

---
+++ LinkedList/Detect_Loop.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+bool detectLoop(Node* head) {
+    if (head == nullptr) return false;
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return true;
+    }
+    return false;
+}
+
+int main() {
+   Node* head = new Node(1);
+    Node* second = new Node(2);
+    Node* third = new Node(3);
+    Node* fourth = new Node(4);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+
+    // Manually create loop: fourth → second
+    fourth->next = second;
+    cout << detectLoop(head);  
+
+    return 0;
+}
